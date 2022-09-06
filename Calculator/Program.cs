40104d955382b2080a71c88d0cd92818524c6d10
Pedro Ceolato <pedroceolato2002@gmail.com>
2022-09-06 17:02:14 -0300

    project Calculator

---
+++ Calculator/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Calculator // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            Console.WriteLine(RetornaTriangulo(15, 7.5));
+        }
+
+        static double RetornaTriangulo(int bse, double altura) 
+        {
+            var area = (bse * altura) / 2;
+            return area;
+        }
+
+    }
+}
