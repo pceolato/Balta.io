981dc876ed3868e40ca5228e8a5a75b11f2463b6
Pedro Ceolato <pedroceolato2002@gmail.com>
2022-09-08 19:12:01 -0300

    project StopWatch

---
+++ StopWatch/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace StopWatch 
+{
+    class Program 
+    {
+        static void main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        } 
+    }
+}
