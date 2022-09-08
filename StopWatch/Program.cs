9a6a5e10225c322a3726b402cc5e8f782e00ddc9
Pedro Ceolato <pedroceolato2002@gmail.com>
2022-09-08 20:36:16 -0300

    completed

--- StopWatch/Program.cs
+++ StopWatch/Program.cs
@@ -1,13 +1,67 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
+using System.Threading;
 
 namespace StopWatch 
 {
     class Program 
     {
-        static void main(string[] args)
+        static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Menu();
+        }
+
+        static void Menu()
+        {
+            Console.Clear();
+            Console.WriteLine("S = Segundo => 10s = 10 segundos");
+            Console.WriteLine("M = Minuto => 1m = 1 minuto");
+            Console.WriteLine("00 = Sair");
+            Console.WriteLine("Quanto tempo deseja contar?");
+
+            string data = Console.ReadLine().ToLower();
+            char type = char.Parse(data.Substring(data.Length - 1, 1));
+            int time = int.Parse(data.Substring(0, data.Length - 1));
+            int multiplier = 1;
+
+            if(type == 'm')
+                multiplier = 60;
+            
+            if(time == 0)
+                System.Environment.Exit(0);
+            
+            PreStart(time * multiplier);
         } 
+
+    static void PreStart(int time) 
+    {
+        Console.Clear();
+        Console.WriteLine("Ready...");
+        Thread.Sleep(1000);
+        Console.WriteLine("Set...");
+        Thread.Sleep(1000);
+        Console.WriteLine("Go!!!");
+        Thread.Sleep(2000);
+
+        Start(time);
+    }
+
+        static void Start(int time)
+        {
+            int currentTime = 0;    
+
+            while (currentTime != time)
+            {
+                Console.Clear();
+                currentTime++;
+                Console.WriteLine(currentTime);
+                Thread.Sleep(1000);
+            }
+
+            Console.Clear();
+            Console.WriteLine("Stopwatch finalizado");
+            Thread.Sleep(2000);
+            Menu();
+        }
     }
 }
