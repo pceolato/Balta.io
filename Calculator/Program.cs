db615614ef4e9a20fb0be3ce15ef31cd24647f60
Pedro Ceolato <pedroceolato2002@gmail.com>
2022-09-08 19:04:51 -0300

    completed

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -7,11 +7,40 @@ namespace Calculator // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Multiplicacao();
-            // Subtracao();
+            Menu();
         }
 
-        static void Soma() {
+        static void Menu()
+        {
+            Console.Clear();
+
+            Console.WriteLine("Olá, o que deseja fazer?");
+            Console.WriteLine("1 - Soma");
+            Console.WriteLine("2 - Subtração");
+            Console.WriteLine("3 - Divisão");
+            Console.WriteLine("4 - Multiplicação");
+            Console.WriteLine("5 - Sair");
+
+            Console.WriteLine("----------");
+
+            Console.WriteLine("Selecione uma opção: ");
+            short res = short.Parse(Console.ReadLine());
+
+
+            switch (res)
+            {
+                case 1: Sum(); break;
+                case 2: Subtraction(); break;
+                case 3: Division(); break;
+                case 4: Multiplication(); break;
+                case 5: Exit(); break;
+                default: Menu(); break;
+
+            }
+        }
+
+        static void Sum()
+        {
             Console.Clear();
 
             Console.WriteLine("Primeiro valor: ");
@@ -23,12 +52,14 @@ namespace Calculator // Note: actual namespace depends on the project name.
             Console.WriteLine("");
 
             float resultado = v1 + v2;
-            Console.WriteLine($"O resultado da soma é {resultado}");
+            Console.WriteLine($"O resultado da somum {resultado}");
 
             Console.ReadKey();
+            Menu();
         }
 
-        static void Subtracao() {
+        static void Subtraction()
+        {
             Console.Clear();
 
             Console.WriteLine("Primeiro valor: ");
@@ -43,9 +74,11 @@ namespace Calculator // Note: actual namespace depends on the project name.
             Console.WriteLine($"O resultado da subtração é {resultado}");
 
             Console.ReadKey();
+            Menu();
         }
 
-        static void Divisao() {
+        static void Division()
+        {
             Console.Clear();
 
             Console.WriteLine("Primeiro valor: ");
@@ -57,12 +90,14 @@ namespace Calculator // Note: actual namespace depends on the project name.
             Console.WriteLine("");
 
             float resultado = v1 / v2;
-            Console.WriteLine($"O resultado da divisão é {resultado}"); 
+            Console.WriteLine($"O resultado da divisão é {resultado}");
 
             Console.ReadKey();
+            Menu();
         }
 
-        static void Multiplicacao() {
+        static void Multiplication()
+        {
             Console.Clear();
 
             Console.WriteLine("Primeiro valor: ");
@@ -77,6 +112,14 @@ namespace Calculator // Note: actual namespace depends on the project name.
             Console.WriteLine($"O resultado da multiplicação é {resultado}");
 
             Console.ReadKey();
+            Menu();
+        }
+
+        static void Exit()
+        {
+            Console.Clear();
+            Console.WriteLine("Obrigado pelo acesso, até mais!");
+            System.Environment.Exit(0);
         }
     }
 }
