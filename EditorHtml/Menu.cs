0b21ef0ccd0cbc28baf39af59d73f10c22310167
Pedro Ceolato <pedroceolato2002@gmail.com>
2022-09-23 11:20:01 -0300

    project EditorHtml

---
+++ EditorHtml/Menu.cs
@@ -0,0 +1,14 @@
+namespace EditorHtml 
+{
+    public static class Program
+    {
+        public static void Show()
+        {
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            
+        }            
+    }
+} 
